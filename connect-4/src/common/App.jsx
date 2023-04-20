fe67f911014d7a579365ebd75ef7428a00ffee71
vicente999 <vicente.delpiano@uc.cl>
2023-04-20 11:04:25 -0400

    landing listo

--- connect-4/src/common/App.jsx
+++ connect-4/src/common/App.jsx
@@ -6,8 +6,28 @@ function App() {
 
   return (
     <div className="App">
-      <h1 id='mainMenu'>connect us</h1>
+      <div><h1 className='title'>connect us</h1></div>
+      <div className='menu'>
+      <div>
+      <form className="email-form">
+      <input type="text" placeholder="email" required />
+      </form>
+      </div>
+      <div>
+      <form className="email-form">
+      <input type="password" placeholder="password" required />
+      </form>
+      </div>
       <div className='botones'>
+        <button>log in</button>
+        <button>sign up</button>
+      </div>
+
+      </div>
+
+      
+
+      {/* <div className='botones'>
       <div>
         <button>log in</button>
         <button>sign up</button>
@@ -18,7 +38,7 @@ function App() {
       </div>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       </div>
-      <h1>{count}</h1>
+      <h1>{count}</h1> */}
     </div>
   )
 }
