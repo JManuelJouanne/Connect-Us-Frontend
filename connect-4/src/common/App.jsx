d2b219337880d7265fea1e1420ccce2f39620fa8
vicente999 <vicente.delpiano@uc.cl>
2023-04-19 23:58:46 -0400

    landing

--- connect-4/src/common/App.jsx
+++ connect-4/src/common/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './../assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,19 @@ function App() {
 
   return (
     <div className="App">
+      <h1 id='mainMenu'>connect us</h1>
+      <div className='botones'>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button>log in</button>
+        <button>sign up</button>
       </div>
-      <h1>Vite - React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id='otro'>
+        <button>about us</button>
+        <button>instructions</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
+      <h1>{count}</h1>
     </div>
   )
 }
