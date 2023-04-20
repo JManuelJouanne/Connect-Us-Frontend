590ca7c1da9cdbb53a2b90c0a5665e833dc70b8b
vicente999 <vicente.delpiano@uc.cl>
2023-04-20 11:59:32 -0400

    avances

--- connect-4/src/common/App.jsx
+++ connect-4/src/common/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
@@ -24,21 +22,6 @@ function App() {
       </div>
 
       </div>
-
-      
-
-      {/* <div className='botones'>
-      <div>
-        <button>log in</button>
-        <button>sign up</button>
-      </div>
-      <div id='otro'>
-        <button>about us</button>
-        <button>instructions</button>
-      </div>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      </div>
-      <h1>{count}</h1> */}
     </div>
   )
 }
