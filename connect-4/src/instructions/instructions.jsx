4b696f4ce180d4be0188d18c8b154c62e8ba8a9c
JOSÉ MANUEL JOUANNE <jmjouanne@uc.cl>
2023-04-21 15:19:41 -0400

    instrucciones dinamicas

--- connect-4/src/instructions/instructions.jsx
+++ connect-4/src/instructions/instructions.jsx
@@ -1,12 +1,35 @@
+import React, {useState} from 'react';
+import './instructions.css';
+
 export default function Instructions() {
+    const [id, setId] = useState(1);
+
+    const next = () => {
+        setId(id + 1);
+    }
+    const previous = () => {
+        setId(id - 1);
+    }
+
+    const instructions = [
+        "dos jugadores se turnarán para ir poniendo fichas en el tablero",
+        "las fichas caerán hacia abajo, por lo que se selecciona una columna, y la ficha cae en el espacio correspondiente",
+        "en cada turno, el jugador podrá colocar una ficha",
+        "el jugador que consiga colocar 4 fichas en línea (horizontal, vertical o diagonal) ¡¡gana!!",
+    ];
+    
     return (
         <>
         <h1>instrucciones</h1>
-        <div className="paragraph">
-            <p>1 - dos jugadores se turnarán para ir poniendo fichas en el tablero</p>
-            <p>2 - las fichas caerán hacia abajo, por lo que se selecciona una columna, y la ficha cae en el espacio correspondiente</p>
-            <p>3 - en cada turno, el jugador podrá colocar una ficha</p>
-            <p>4 - el jugador que consiga colocar 4 fichas en línea (horizontal, vertical o diagonal) ¡¡gana!!</p>
+        <div className='rule'>
+            <div className='rule-content'>
+                <h2 className='n-rule'>{id}</h2>
+                <p>{instructions[id - 1]}</p>
+            </div>
+            <div className='button-container'>
+                {id!=1 && <button onClick={previous} className="next-button">&lt;&lt;</button>}
+                {id!=4 && <button onClick={next} className="next-button">&gt;&gt;</button>}
+            </div>
         </div>
         <a href="/principal">volver</a>
         </>
